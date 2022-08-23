647972e9d861e0201b2ed5129005d0732b6aa14a
Andrew Heath <andrew@heath.org>
2022-08-23 17:02:17 +0200

    run once an hour

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config()
 
 const headless = true;
-schedule.scheduleJob('30 * * * * *', async function() {
+schedule.scheduleJob('* 1 * * *', async function() {
     let newKamernetPosts, newRoomzPosts, failed = [];
     try{
         newKamernetPosts = await kamernetScrapper(headless)
